0c621623eda6e673709ebb3a8276ded0f4cd4e1b
RadhapyariDevi <radhapyarikhuman123@gmail.com>
2026-04-09 19:41:03 +0530

    Initial commit

--- src/server.js
+++ src/server.js
@@ -1,5 +1,10 @@
 import express from "express";
 import movieRoutes from "./routes/movieRoutes.js"
+import {config} from 'dotenv'
+import {connectDB, disconnectDB} from "./config/db.js"
+
+config();
+connectDB();
 
 const app = express()
 
@@ -9,6 +14,30 @@ const PORT = 5001;
 
 const server = app.listen(PORT, ()=>{
     console.log(`Server running on PORT ${PORT}`);
+});
+
+process.on("unhandledRejection", (err) => {
+    console.error("Unhandled Rejection: ", err);
+    server.close(async () => {
+        await disconnectDB();
+        process.exit(1);
+    });
+});
+
+process.on("uncaughtException", async (err) => {
+    console.error("Uncaught Exception: ", err);
+    server.close(async () => {
+        await disconnectDB();
+        process.exit(1);
+    });
+});
+
+process.on("SIGTERM",async  () => {
+    console.error("SIGTERM received, shutting down gracefully");
+    server.close(async () => {
+        await disconnectDB();
+        process.exit(0);
+    });
 });
 
 
