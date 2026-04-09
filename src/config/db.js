41fcf316021d7e628bb6e39f3ea303975e2b6bb8
RadhapyariDevi <radhapyarikhuman123@gmail.com>
2026-04-09 21:57:41 +0530

    Add controller logic n fixed prisma v7 issues

--- src/config/db.js
+++ src/config/db.js
@@ -1,25 +1,32 @@
-import { PrismaClient } from "@prisma/client";
+import pkg from "@prisma/client";
+const { PrismaClient } = pkg;
+
+import { PrismaPg } from "@prisma/adapter-pg";
+
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL,
+});
 
 const prisma = new PrismaClient({
+  adapter, 
   log:
     process.env.NODE_ENV === "development"
       ? ["query", "error", "warn"]
       : ["error"],
 });
 
-
-const connectDB =  async () =>{
-    try{
-        await prisma.$connect();
-        console.log("DB connected via Prisma");
-    } catch(error){
-        console.error(`Database connection error: ${error.message}`);
-        process.exit(1);
-    }
+const connectDB = async () => {
+  try {
+    await prisma.$connect();
+    console.log("DB connected via Prisma");
+  } catch (error) {
+    console.error(`Database connection error: ${error.message}`);
+    process.exit(1);
+  }
 };
 
-const disconnectDB =  async () =>{
-    await prisma.$disconnect();
-}
+const disconnectDB = async () => {
+  await prisma.$disconnect();
+};
 
-export {prisma, connectDB, disconnectDB};
+export { prisma, connectDB, disconnectDB };
