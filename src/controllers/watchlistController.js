9858dd5ac78288e41b3e49263e86acedcea8bdff
RadhapyariDevi <radhapyarikhuman123@gmail.com>
2026-04-13 15:19:40 +0530

    Add routes + controller for deleting & updating item from watchlist

--- src/controllers/watchlistController.js
+++ src/controllers/watchlistController.js
@@ -45,4 +45,60 @@ const addToWatchlist = async (req,res)=> {
 
 };
 
-export {addToWatchlist};
+const removeFromWatchlist = async (req,res) => {
+    const item = await prisma.watchlistItem.findUnique({
+        where : {
+            id: req.params.id
+        },
+    });
+    if(!item){
+        return res.status(400).json({error:"Item not found"});
+    }
+
+    if(item.userId !== req.user.id){
+        return res.status(401).json({error:"Not allowed to delete this item"});
+    }
+
+    await prisma.watchlistItem.delete({
+        where: {id: req.params.id},
+    });
+
+    res.status(200).json({
+        status:"Success",
+        messsage:"Item is removed successfully",
+    });
+};
+
+const updateIttemInWatchlist = async(req,res) => {
+    const {status, rating} = req.body;
+    const item = await prisma.watchlistItem.findUnique({
+        where : {
+            id: req.params.id
+        },
+    });
+
+    if(!item){
+        return res.status(400).json({error:"Item not found"});
+    }
+
+    if(item.userId !== req.user.id){
+        return res.status(401).json({error:"Not allowed to update this item"});
+    }
+
+    const updateData = {};
+    if(status !== undefined) updateData.status = status.toUpperCase();
+    if(rating !== undefined) updateData.rating = rating;
+
+    const updateItem = await prisma.watchlistItem.update({
+        where: {id:req.params.id},
+        data:updateData,
+    })
+
+    res.status(200).json({
+        status:"Success",
+        messsage:"Item is removed successfully",
+        data: updateItem,
+    });
+}
+
+export {addToWatchlist, removeFromWatchlist, updateIttemInWatchlist};
--- src/routes/watchlistRoutes.js
+++ src/routes/watchlistRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import {addToWatchlist} from "../controllers/watchlistController.js";
+import {addToWatchlist, removeFromWatchlist, updateIttemInWatchlist} from "../controllers/watchlistController.js";
 import { authMiddleware } from "../middleware/authmiddleware.js";
 
 const router = express.Router();
@@ -7,6 +7,10 @@ const router = express.Router();
 router.use(authMiddleware);
 
 router.post("/" , addToWatchlist);
+
+router.delete("/:id", removeFromWatchlist);
+
+router.put("/:id", updateIttemInWatchlist);
 
 
 export default router;
