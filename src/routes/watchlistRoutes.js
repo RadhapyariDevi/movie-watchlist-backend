3906fee9b2f21a5b66adac5c611a04dc71633e9e
RadhapyariDevi <radhapyarikhuman123@gmail.com>
2026-04-14 18:18:17 +0530

    Add validator for adding movie to watchlist

--- src/routes/watchlistRoutes.js
+++ src/routes/watchlistRoutes.js
@@ -1,12 +1,14 @@
 import express from "express";
 import {addToWatchlist, removeFromWatchlist, updateIttemInWatchlist} from "../controllers/watchlistController.js";
 import { authMiddleware } from "../middleware/authmiddleware.js";
+import { validateRequest } from "../middleware/validateRequest.js";
+import { addToWatchlistSchema } from "../validators/watchlistValidators.js";
 
 const router = express.Router();
 
 router.use(authMiddleware);
 
-router.post("/" , addToWatchlist);
+router.post("/" , validateRequest(addToWatchlistSchema), addToWatchlist);
 
 router.delete("/:id", removeFromWatchlist);
 
