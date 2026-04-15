de86a19749de62cfe1ea5eefce6b9288d5ddaa54
RadhapyariDevi <radhapyarikhuman123@gmail.com>
2026-04-15 21:31:51 +0530

    Minor modifications to deploy in render

--- src/server.js
+++ src/server.js
@@ -24,7 +24,7 @@ app.use("/movies", movieRoutes);
 app.use("/auth", authRoutes);
 app.use("/watchlist", watchlistRoutes);
 
-const PORT = 5001;
+const PORT = process.env.PORT || 5000;
 
 
 const server = app.listen(PORT, ()=>{
